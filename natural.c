ef221f35b0804c5ec731f489fd3ee61f2e5c1e78
suryakumar134 <35790803+suryakumar134@users.noreply.github.com>
2018-01-30 16:46:52 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,20 @@
+
+    #include <stdio.h>
+int main()
+{
+    int n, i, sum = 0;
+    
+    printf("Enter a positive number: ");
+    scanf("%d",&n);
+
+    for(i=1; i <= n; ++i)
+    {
+        sum += i;   // sum = sum+i;
+    }
+
+    printf("Sum = %d",sum);
+
+    return 0;
+}
+        
+   
