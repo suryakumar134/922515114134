a01b050627845a3afbe8061bb1dcdde7ed579531
suryakumar134 <35790803+suryakumar134@users.noreply.github.com>
2018-02-06 16:46:58 +0530

    Create sarah.c

---
+++ sarah.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    // True if the number is perfectly divisible by 2
+    if(number % 2 == 0)
+        printf("%d is even.", number);
+    else
+        printf("%d is odd.", number);
+
+}
