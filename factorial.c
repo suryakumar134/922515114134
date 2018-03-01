7a58a7e9aa42914048ed14b48e5023da73e9c447
suryakumar134 <35790803+suryakumar134@users.noreply.github.com>
2018-03-01 16:06:33 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,fact=1;
+    printf("enter the integer");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        fact*=i;
+    }
+     printf("%d",fact);
+    
+}
+
